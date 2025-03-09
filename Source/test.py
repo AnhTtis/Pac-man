b6533997e9372c3544676060c61fb90245fafa08
lazymingg <nvminh232@clc.fitus.edu.vn>
2025-03-09 10:38:45 +0700

    not debug yet

--- Source/test.py
+++ Source/test.py
@@ -3,8 +3,30 @@ from Ghost import Ghost
 from Maze import Maze
 import time
 from Ghost import Ghost, BlueGhost, OrangeGhost, PinkGhost
+from GhostHandle import GameManager
+from Pacman import Pacman
+import copy
+import threading
 
 #lazymingg chao cau nheeeee <3
+
+# maze_grid = [
+#         [' ', ' ', ' ', '#', ' ', ' '],
+#         [' ', '#', ' ', '#', ' ', '#'],
+#         [' ', '#', ' ', ' ', ' ', 'P'],
+#         [' ', ' ', '#', '#', '#', ' '],
+#         [' ', ' ', ' ', ' ', ' ', ' ']
+#     ]
+
+# maze = Maze(maze_grid)
+# # maze.load_pacman()
+# # ghost = BlueGhost(maze, (3, 4))
+# ghost = PinkGhost(maze, (5, 3))
+# start_time = time.perf_counter()
+# path = ghost.move((0, 0))
+# end_time = time.perf_counter()
+# print("Time: ", end_time - start_time)
+# print(path)
 
 maze_grid = [
         [' ', ' ', ' ', '#', ' ', ' '],
@@ -13,13 +35,9 @@ maze_grid = [
         [' ', ' ', '#', '#', '#', ' '],
         [' ', ' ', ' ', ' ', ' ', ' ']
     ]
-
 maze = Maze(maze_grid)
-# maze.load_pacman()
-# ghost = BlueGhost(maze, (3, 4))
-ghost = PinkGhost(maze, (5, 3))
-start_time = time.perf_counter()
-path = ghost.move((0, 0))
-end_time = time.perf_counter()
-print("Time: ", end_time - start_time)
-print(path)
+pacman = Pacman(maze, (2, 5))
+ghosts = [BlueGhost(maze, (3, 4)), PinkGhost(maze, (5, 3))]
+positions = {'BlueGhost': (3, 4), 'PinkGhost': (5, 3)}
+game = GameManager(maze, (2, 5), ghosts, positions)
+print(game.get_pacman_pos())
