9b21ff7e35780750f69dd0e50ada6fb085bdd1c0
TKin <letrungkienthd@gmail.com>
2025-03-13 21:54:24 +0700

    .

--- Source/ghosts/ghost.py
+++ Source/ghosts/ghost.py
@@ -1,59 +1,58 @@
 import pygame
+from typing import List, Tuple, Dict, Set, Optional
+from maze import Maze
 
 class Ghost:
-    def __init__(self, pos, width, height, color):
+    def __init__(self, name, maze: 'Maze', pos: List[int, int], size: Tuple[int, int], color):
+        self.name = name
+        self.maze = maze
+
         self.pos = pos #[x, y]
-        self.width = width
-        self.height = height
+        self.sie = size
+        
         self.color = color
-        self.face_left = True
+        self.face_right = False
         self.moving = False
         self.appearance = None
 
     def move(self, dx, dy):
         if dx != 0:
             self.pos[0] += dx
-            if dx > 0 and self.face_left:
-                self.face_left = False
-            elif dx < 0 and not self.face_left:
-                self.face_left = True
+            if dx < 0 and self.face_right:
+                self.face_right = False
+            elif dx > 0 and not self.face_right:
+                self.face_right = True
         self.pos[1] += dy
         self.moving = not self.moving
 
     def display(self, screen):
         if self.appearance:
-            screen.blit(self.appearance[self.face_left][self.moving], (self.pos[0], self.pos[1]))
+            screen.blit(self.appearance[self.face_right], self.pos)
 
 
 class Blue(Ghost):
     def __init__(self, pos, width, height):
         super().__init__(pos, width, height, (0, 0, 255))
-        self.appearance = [
-            [pygame.transform.scale(pygame.image.load("blue1_flip.png"), (self.width, self.height)), pygame.transform.scale(pygame.image.load("blue2_flip.png"), (self.width, self.height))],
-            [pygame.transform.scale(pygame.image.load("blue1.png"), (self.width, self.height)), pygame.transform.scale(pygame.image.load("blue2.png"), (self.width, self.height))]
-        ]
+        self.appearance = [pygame.transform.scale(pygame.image.load("blue1.png"), (20,20))]
+        self.appearance.append(pygame.transform.flip(self.appearance[0], True, False))
 
 class Red(Ghost):
     def __init__(self, pos, width, height):
         super().__init__(pos, width, height, (255, 0, 0))
-        self.appearance = [
-            [pygame.transform.scale(pygame.image.load("red1_flip.png"), (self.width, self.height)), pygame.transform.scale(pygame.image.load("red2_flip.png"), (self.width, self.height))],
-            [pygame.transform.scale(pygame.image.load("red1.png"), (self.width, self.height)), pygame.transform.scale(pygame.image.load("red2.png"), (self.width, self.height))]
-        ]
+        self.appearance = [pygame.transform.scale(pygame.image.load("red1.png"), (20,20))]
+        self.appearance.append(pygame.transform.flip(self.appearance[0], True, False))
+
 
 class Pink(Ghost):
     def __init__(self, pos, width, height):
         super().__init__(pos, width, height, (255, 105, 180))
-        self.appearance = [
-            [pygame.transform.scale(pygame.image.load("pink1_flip.png"), (self.width, self.height)), pygame.transform.scale(pygame.image.load("pink2_flip.png"), (self.width, self.height))],
-            [pygame.transform.scale(pygame.image.load("pink1.png"), (self.width, self.height)), pygame.transform.scale(pygame.image.load("pink2.png"), (self.width, self.height))]
-        ]
+        self.appearance = [pygame.transform.scale(pygame.image.load("pink1.png"), (20,20))]
+        self.appearance.append(pygame.transform.flip(self.appearance[0], True, False))
 
 
-class Yellow(Ghost):
+
+class Orange(Ghost):
     def __init__(self, pos, width, height):
         super().__init__(pos, width, height, (255, 255, 0))
-        self.appearance = [
-            [pygame.transform.scale(pygame.image.load("yellow1_flip.png"), (self.width, self.height)), pygame.transform.scale(pygame.image.load("yellow2_flip.png"), (self.width, self.height))],
-            [pygame.transform.scale(pygame.image.load("yellow1.png"), (self.width, self.height)), pygame.transform.scale(pygame.image.load("yellow2.png"), (self.width, self.height))]
-        ]
+        self.appearance = [pygame.transform.scale(pygame.image.load("yellow1.png"), (20,20))]
+        self.appearance.append(pygame.transform.flip(self.appearance[0], True, False))
--- Source/ghosts/testghost.py
+++ Source/ghosts/testghost.py
@@ -4,6 +4,8 @@ import ghost
 
 # Khởi tạo Pygame và vẽ con ma
 pygame.init()
+FPS = 60
+clock = pygame.time.Clock()
 
 # Kích thước cửa sổ
 screen_info = pygame.display.Info()
@@ -19,10 +21,11 @@ BLOCK_SIZE = HEIGHT / 40    # Kích thước mỗi ô
 blue_ghost = ghost.Blue([50, 50], BLOCK_SIZE, BLOCK_SIZE)
 red_ghost = ghost.Red([100, 50], BLOCK_SIZE, BLOCK_SIZE)
 pink_ghost = ghost.Pink([150, 50], BLOCK_SIZE, BLOCK_SIZE)
-yellow_ghost = ghost.Yellow([200, 50], BLOCK_SIZE, BLOCK_SIZE)
+orange_ghost = ghost.Orange([200, 50], BLOCK_SIZE, BLOCK_SIZE)
 
 running = True
 while running:
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -33,7 +36,7 @@ while running:
     blue_ghost.display(screen)
     red_ghost.display(screen)
     pink_ghost.display(screen)
-    yellow_ghost.display(screen)
+    orange_ghost.display(screen)
     
     blue_ghost.move(1, 0)
 
