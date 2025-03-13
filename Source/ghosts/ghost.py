8704594edb08a1ae21b8700cd64d18db0733e590
TKin <letrungkienthd@gmail.com>
2025-03-13 23:04:00 +0700

    added para to ghost

--- Source/ghosts/ghost.py
+++ Source/ghosts/ghost.py
@@ -1,15 +1,14 @@
 import pygame
-from typing import List, Tuple, Dict, Set, Optional, LiteralString
+from typing import List, Tuple, Dict, Set, Optional
 from maze import Maze
 import copy
 
 class Ghost:
-    def __init__(self, name: LiteralString, pos: List[int], size: Tuple[int, int], color):
+    def __init__(self, name: str, maze: Maze, pos: List[int], size: Tuple[int, int], color):
         self.name = name
-
+        self.maze = maze
         self.pos = pos #[x, y]
         self.size = size
-        self.maze = copy.deepcopy(Maze)
         
         self.color = color
         self.face_right = False
@@ -39,25 +38,25 @@ class Ghost:
             screen.blit(self.appearance[self.face_right], self.pos)
 
 class BlueGhost(Ghost):
-    def __init__(self, name: LiteralString, pos: List[int], size: Tuple[int, int]):
-        super().__init__(name, pos, size, (0, 0, 255))
+    def __init__(self, name: str, maze: Maze, pos: List[int], size: Tuple[int, int]):
+        super().__init__(name, maze, pos, size, (0, 0, 255))
         self.appearance = [pygame.transform.scale(pygame.image.load("blue.png"), self.size)]
         self.appearance.append(pygame.transform.flip(self.appearance[0], True, False))
 
 class RedGhost(Ghost):
-    def __init__(self, name: LiteralString, pos: List[int], size: Tuple[int, int]):
-        super().__init__(name, pos, size, (255, 0, 0))
+    def __init__(self, name: str, maze: Maze, pos: List[int], size: Tuple[int, int]):
+        super().__init__(name, maze, pos, size, (255, 0, 0))
         self.appearance = [pygame.transform.scale(pygame.image.load("red.png"), self.size)]
         self.appearance.append(pygame.transform.flip(self.appearance[0], True, False))
 
 class PinkGhost(Ghost):
-    def __init__(self, name: LiteralString, pos: List[int], size: Tuple[int, int]):
-        super().__init__(name, pos, size, (255, 105, 180))
+    def __init__(self, name: str, maze: Maze, pos: List[int], size: Tuple[int, int]):
+        super().__init__(name, maze, pos, size, (255, 105, 180))
         self.appearance = [pygame.transform.scale(pygame.image.load("pink.png"), self.size)]
         self.appearance.append(pygame.transform.flip(self.appearance[0], True, False))
 
 class OrangeGhost(Ghost):
-    def __init__(self, name: LiteralString, pos: List[int], size: Tuple[int, int]):
-        super().__init__(name, pos, size, (255, 255, 0))
+    def __init__(self, name: str, maze: Maze, pos: List[int], size: Tuple[int, int]):
+        super().__init__(name, maze, pos, size, (255, 255, 0))
         self.appearance = [pygame.transform.scale(pygame.image.load("yellow.png"), self.size)]
         self.appearance.append(pygame.transform.flip(self.appearance[0], True, False))
